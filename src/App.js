8df8f27d5207fc536f1c0f71125849b39cdf5065
laptevvv <tlvea(42545+>
2022-06-14 22:44:46 +0800

    with treeview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { Container, Row, Nav, Tab } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 // import "./App.css";
 import ProjectChoose from "./components/ProjectChoose";
 import LocatorCatTabs from "./components/LocatorCatTabs";
-import TreeView from "./components/TreeView";
+// import TreeView from "./components/TreeView";
 import React from "react";
+import treeData from "./components/TreeView";
+import TreeMenu from "react-simple-tree-menu";
 
 function App() {
   return (
     <Container>
       <Row className="mt-2">
         <ProjectChoose />
-        {/* <Col md={9}>
-          {/* <BrandBar /> */}
-        {/* </Col> */}
       </Row>
       <div className="container-fluid d-flex">
         <div className="col-6 border border-dark border-opacity-25 rounded me-2 p-2">
-          <TreeView />
+          <TreeMenu data={treeData} />
         </div>
         <div className="col-6">
           <div className="container">
